dbfb33e6fb6a92a5ff24d7c9f82e94f07372af47
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-26 15:32:34 +0100

    User Controller

---
+++ controllers/user.controller.js
@@ -0,0 +1,29 @@
+const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json({ message: 'Utilisateur créé avec succès' });
+  } catch (error) {
+    res.status(400).json({ error: 'Erreur lors de l\'inscription' });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ error: 'Identifiants invalides' });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token, user });
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur lors de la connexion' });
+  }
+};
