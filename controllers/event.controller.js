50f507fcfbe535581e3a3523034fa91941b953a8
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-26 15:34:56 +0100

    Event Controller

---
+++ controllers/event.controller.js
@@ -0,0 +1,20 @@
+const Event = require('../models/event.model');
+
+exports.createEvent = async (req, res) => {
+  try {
+    const event = new Event(req.body);
+    await event.save();
+    res.status(201).json(event);
+  } catch (error) {
+    res.status(400).json({ error: 'Erreur lors de la création de l\'événement' });
+  }
+};
+
+exports.getEvents = async (req, res) => {
+  try {
+    const events = await Event.find();
+    res.json(events);
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur lors de la récupération des événements' });
+  }
+};
